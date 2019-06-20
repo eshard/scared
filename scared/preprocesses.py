80211b66d79ef9a09aafcc527632b4eeb4dd3bde
Rémi Huguet <remi.huguet@eshard.com>
2019-06-20 10:13:09 +0000

    feat: Add basic preprocesses - square, fft modulus, serialize bits, center and standardization.

--- scared/preprocesses.py
+++ scared/preprocesses.py
@@ -30,3 +30,122 @@ def preprocess(function):
             raise PreprocessError(f'Preprocess {function} modifies number of traces dimension.')
         return result
     return _
+
+
+@preprocess
+def square(traces):
+    """Returns traces square.
+
+    Args:
+        traces (numpy.ndarray): a 2 dimensional numpy array.
+
+    Returns:
+        (numpy.ndarray) square of input traces array.
+
+    """
+    return traces ** 2
+
+
+@preprocess
+def serialize_bit(traces):
+    """Returns serialized bit of bytes traces.
+
+    Args:
+        traces (numpy.ndarray): a 2 dimensional numpy array. If traces is not of uint8 dtype,
+            it will be casted to uint8 before serializing bits.
+
+    Returns:
+        (numpy.ndarray) serialized bits along dimension 2 of input traces array.
+
+    """
+    return _np.unpackbits(traces.astype('uint8'), axis=1)
+
+
+@preprocess
+def fft_modulus(traces):
+    """Returns the modulus of FFT of input traces.
+
+    Due to symmetry of the modulus of FFT, only the first half of computing modulus traces are returned.
+
+    Args:
+        traces (numpy.ndarray): a 2 dimensional numpy array.
+
+    Returns:
+        (numpy.ndarray) the modulus of FFT applied on traces.
+
+    """
+    length = _np.ceil(traces.shape[1] / 2).astype('uint32')
+    return _np.abs(_np.fft.fft(traces))[:, :length]
+
+
+def _center(traces, mean):
+    try:
+        return traces - mean
+    except ValueError:
+        raise PreprocessError(f'Incompatible shapes between traces {traces.shape} and mean {mean.shape}.')
+
+
+@preprocess
+def center(traces):
+    """Returns traces centered around the mean of all traces.
+
+    Args:
+        traces (numpy.ndarray): a 2 dimensional numpy array.
+
+    Returns:
+        (numpy.ndarray) traces substracted of the mean on all traces.
+
+    """
+    return _center(traces, _np.nanmean(traces, axis=0))
+
+
+@preprocess
+def standardize(traces):
+    """Returns traces centered around the mean of all traces, and normalized on the standard of all traces.
+
+    Args:
+        traces (numpy.ndarray): a 2 dimensional numpy array.
+
+    Returns:
+        (numpy.ndarray) traces substracted of the mean on all traces and normalized on the standard of all traces.
+
+    """
+    return center(traces) / _np.nanstd(traces, axis=0)
+
+
+def standardize_on(mean=None, std=None):
+    """Returns a preprocess to compute standardization around provided mean and std values.
+
+    If mean (resp. std) is not provided, the preprocess will use the mean (resp. std) value processed on traces.
+
+    Args:
+        mean (numpy.ndarray, default=None): use this array as mean value to center traces.
+        std (numpy.ndarray, default=None): use this array as std value to normalize traces.
+
+    Returns:
+        (callable): preprocess function to compute standardization of traces around mean and std.
+
+    """
+    def _(traces):
+        _mean = mean if mean is not None else _np.nanmean(traces, axis=0)
+        _std = std if std is not None else _np.nanstd(traces, axis=0)
+        try:
+            return (traces - _mean) / _std
+        except ValueError:
+            raise PreprocessError(f'Incompatible shapes between traces {traces.shape} and mean {_mean.shape} and/or std {_std.shape}.')
+    return preprocess(_)
+
+
+def center_on(mean):
+    """Returns a preprocess to compute centering traces around provided mean.
+
+    Args:
+        mean (numpy.ndarray): use this array as mean value to center traces.
+
+    Returns:
+        (callable): preprocess function to compute center of traces around mean.
+
+    """
+    def _(traces):
+        return _center(traces, mean)
+    return preprocess(_)
---
+++ tests/test_preprocesses_simple.py
@@ -0,0 +1,103 @@
+from .context import scared
+import pytest
+import numpy as np
+
+
+@pytest.fixture(params=['int', 'uint', 'float'])
+def traces(request):
+    if request.param == 'int':
+        return np.random.randint(-10000, 10000, (500, 2001), dtype='int16')
+    elif request.param == 'uint':
+        return np.random.randint(0, 256, (500, 2001), dtype='uint8')
+    elif request.param == 'float':
+        return np.random.random((500, 2001))
+
+
+def test_square_preprocess(traces):
+    assert np.array_equal(traces ** 2, scared.preprocesses.square(traces))
+
+    with pytest.raises(TypeError):
+        scared.preprocesses.square('foo')
+
+
+def test_serialize_bit_preprocess(traces):
+    assert np.array_equal(
+        np.unpackbits(traces.astype('uint8'), axis=1),
+        scared.preprocesses.serialize_bit(traces))
+
+    with pytest.raises(TypeError):
+        scared.preprocesses.serialize_bit('foo')
+
+
+def test_fft_modulus_preprocess(traces):
+    le = np.ceil(traces.shape[1] / 2).astype('uint32')
+    expected = np.abs(np.fft.fft(traces))[:, :le]
+    assert np.array_equal(
+        expected,
+        scared.preprocesses.fft_modulus(traces))
+
+    with pytest.raises(TypeError):
+        scared.preprocesses.fft_modulus('foo')
+
+
+def test_center(traces):
+    expected = traces - np.mean(traces, axis=0)
+    result = scared.preprocesses.center(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_center_on_given_mean(traces):
+    given_mean = np.mean(np.random.random((500, 2001)), axis=0)
+    expected = traces - given_mean
+    result = scared.preprocesses.center_on(mean=given_mean)(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_center_on_given_mean_raises_exception_if_incompatible_shapes(traces):
+    wrong_mean = np.mean(traces, axis=1)
+
+    with pytest.raises(scared.PreprocessError):
+        scared.preprocesses.center_on(wrong_mean)(traces)
+
+
+def test_standardize(traces):
+    expected = traces - np.mean(traces, axis=0)
+    expected /= np.std(traces, axis=0)
+    result = scared.preprocesses.standardize(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_standardize_on_given_mean(traces):
+    given_mean = np.mean(np.random.random((500, 2001)), axis=0)
+    expected = (traces - given_mean) / np.std(traces, axis=0)
+    result = scared.preprocesses.standardize_on(mean=given_mean)(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_standardize_on_given_std(traces):
+    given_std = np.nanstd(np.random.random((500, 2001)), axis=0)
+    expected = (traces - np.mean(traces, axis=0)) / given_std
+    result = scared.preprocesses.standardize_on(std=given_std)(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_standardize_on_given_std_and_mean(traces):
+    given_mean = np.nanmean(np.random.random((500, 2001)), axis=0)
+    given_std = np.nanstd(np.random.random((500, 2001)), axis=0)
+    expected = (traces - given_mean) / given_std
+    result = scared.preprocesses.standardize_on(std=given_std, mean=given_mean)(traces)
+    assert np.array_equal(expected, result)
+
+
+def test_standardize_on_raises_exception_if_incompatible_shapes(traces):
+    wrong_mean = np.mean(traces, axis=1)
+    wrong_std = np.std(traces, axis=1)
+
+    with pytest.raises(scared.PreprocessError):
+        scared.preprocesses.standardize_on(mean=wrong_mean)(traces)
+
+    with pytest.raises(scared.PreprocessError):
+        scared.preprocesses.standardize_on(std=wrong_std)(traces)
+
+    with pytest.raises(scared.PreprocessError):
+        scared.preprocesses.standardize_on(std=wrong_std, mean=wrong_mean)(traces)
