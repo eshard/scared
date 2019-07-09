4010dd42fc697179100b697757e72815123ec18c
Rémi Huguet <remi.huguet@eshard.com>
2019-07-09 15:07:20 +0200

    maint: Prepare 0.3.0 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@ copyright = '2019, eshard'
 author = 'eshard'
 
 # The short X.Y version
-version = '0.2.0'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0a'
+release = '0.3.0a'
 
 
 # -- General configuration ---------------------------------------------------
