e33f680c13d666b6c472366abaf374ca629be950
Rémi Huguet <remi.huguet@eshard.com>
2019-08-29 15:25:20 +0200

    maint: Prepare 0.6.3 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@ copyright = '2019, eshard'
 author = 'eshard'
 
 # The short X.Y version
-version = '0.6.2'
+version = '0.6.3'
 # The full version, including alpha/beta/rc tags
-release = '0.6.2'
+release = '0.6.3'
 
 
 # -- General configuration ---------------------------------------------------
