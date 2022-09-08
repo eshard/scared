5112ffba41bb250f6b4005afd5f02ed8dad4408d
Aurélien Vasselle <aurelien.vasselle@eshard.com>
2022-09-08 17:49:39 +0200

    replace np.min/max with python min/max

--- scared/des/selection_functions/decrypt.py
+++ scared/des/selection_functions/decrypt.py
@@ -9,7 +9,7 @@ FirstAddRoundKey.__doc__ = """Build an attack selection function which computes 
         ciphertext_tag (str, default='ciphertext'): tag (key value) of the ciphertext metadata to use to retrieve ciphertext
             values from the metadata dict when selection function is called.
 
-    """
+    """  # noqa E501
 
 LastAddRoundKey = encrypt.FirstAddRoundKey
 LastAddRoundKey.__doc__ = """Build an attack selection function which computes intermediate values after DES decrypt round key operation at last round, for guesses values.
@@ -20,7 +20,7 @@ LastAddRoundKey.__doc__ = """Build an attack selection function which computes i
         plaintext_tag (str, default='plaintext'): tag (key value) of the plaintext metadata to use to retrieve plaintext
             values from the metadata dict when selection function is called.
 
-    """
+    """  # noqa E501
 
 FirstSboxes = encrypt.LastSboxes
 FirstSboxes.__doc__ = """Build an attack selection function which computes intermediate values after DES decrypt sub bytes (S-box) operation at first round, for guesses values.
@@ -31,7 +31,7 @@ FirstSboxes.__doc__ = """Build an attack selection function which computes inter
         ciphertext_tag (str, default='ciphertext'): tag (key value) of the ciphertext metadata to use to retrieve ciphertext
             values from the metadata dict when selection function is called.
 
-    """
+    """  # noqa E501
 
 LastSboxes = encrypt.FirstSboxes
 LastSboxes.__doc__ = """Build an attack selection function which computes intermediate values after DES decrypt sub bytes (S-box) operation at last round, for guesses values.
@@ -42,7 +42,7 @@ LastSboxes.__doc__ = """Build an attack selection function which computes interm
         plaintext_tag (str, default='plaintext'): tag (key value) of the plaintext metadata to use to retrieve plaintext
             values from the metadata dict when selection function is called.
 
-    """
+    """  # noqa E501
 
 FeistelRFirstRounds = encrypt.FeistelRLastRounds
 FeistelRFirstRounds.__doc__ = """Build an attack selection function which computes intermediate values after DES Feistel R, for guesses values.
@@ -52,7 +52,7 @@ FeistelRFirstRounds.__doc__ = """Build an attack selection function which comput
         words (:class:`scared.traces.Samples.SUPPORTED_INDICES_TYPES`, default=None): selection of key words computed.
         ciphertext_tag (str, default='ciphertext'): tag (key value) of the ciphertext metadata to use to retrieve ciphertext
             values from the metadata dict when selection function is called.
-    """
+    """  # noqa E501
 
 FeistelRLastRounds = encrypt.FeistelRFirstRounds
 FeistelRLastRounds.__doc__ = """Build an attack selection function which computes intermediate values after DES Feistel R, for guesses values.
@@ -62,7 +62,7 @@ FeistelRLastRounds.__doc__ = """Build an attack selection function which compute
         words (:class:`scared.traces.Samples.SUPPORTED_INDICES_TYPES`, default=None): selection of key words computed.
         plaintext_tag (str, default='plaintext'): tag (key value) of the plaintext metadata to use to retrieve plaintext
             values from the metadata dict when selection function is called.
-    """
+    """  # noqa E501
 
 DeltaRFirstRounds = encrypt.DeltaRLastRounds
 DeltaRFirstRounds.__doc__ = """Build an attack selection function which computes delta intermediate values between DES decrypt first two rounds, for guesses values.
@@ -72,7 +72,7 @@ DeltaRFirstRounds.__doc__ = """Build an attack selection function which computes
         words (:class:`scared.traces.Samples.SUPPORTED_INDICES_TYPES`, default=None): selection of key words computed.
         ciphertext_tag (str, default='ciphertext'): tag (key value) of the ciphertext metadata to use to retrieve ciphertext
             values from the metadata dict when selection function is called.
-    """
+    """  # noqa E501
 
 DeltaRLastRounds = encrypt.DeltaRFirstRounds
 DeltaRLastRounds.__doc__ = """Build an attack selection function which computes delta intermediate values between DES decrypt first two rounds, for guesses values.
@@ -82,4 +82,4 @@ DeltaRLastRounds.__doc__ = """Build an attack selection function which computes 
         words (:class:`scared.traces.Samples.SUPPORTED_INDICES_TYPES`, default=None): selection of key words computed.
         plaintext_tag (str, default='plaintext'): tag (key value) of the plaintext metadata to use to retrieve plaintext
             values from the metadata dict when selection function is called.
-    """
+    """  # noqa E501
