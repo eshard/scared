e5ed0a1e36a75980a336b6c37e56d0aab4d73822
SkapinTheFourb's <florian.boudinet@eshard.com>
2022-09-22 17:50:40 +0200

    WIP

--- scared/_utils.py
+++ scared/_utils.py
@@ -4,6 +4,6 @@ import numpy as _np
 def _is_bytes_array(array):
     if not isinstance(array, _np.ndarray):
         raise TypeError(f'array should be a Numpy ndarray instance, not {type(array)}.')
-    if not (_np.alltrue(array >= 0) and _np.alltrue(array < 256)):
+    if not (_np.alltrue(array >= 0) and _np.alltrue(array < 256)) or not isinstance( in /arra):
         raise ValueError(f'array should be a byte array with uint8 or at least, between 0 and 256 dtype, not {array.dtype}.')
     return True
