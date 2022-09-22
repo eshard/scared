4d21d95ab0b3377f471ad1f0aa89b30841b5d84c
SkapinTheFourb's <florian.boudinet@eshard.com>
2022-09-22 17:33:19 +0200

    [FIX] Inconsistent Support of UINT32 metadata

--- scared/_utils.py
+++ scared/_utils.py
@@ -4,6 +4,6 @@ import numpy as _np
 def _is_bytes_array(array):
     if not isinstance(array, _np.ndarray):
         raise TypeError(f'array should be a Numpy ndarray instance, not {type(array)}.')
-    if not array.dtype == 'uint8':
-        raise ValueError(f'array should be a byte array with uint8 dtype, not {array.dtype}.')
+    if not (_np.alltrue(array >= 0) and _np.alltrue(array < 256)):
+        raise ValueError(f'array should be a byte array with uint8 or at least, between 0 and 256 dtype, not {array.dtype}.')
     return True
