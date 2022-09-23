58cd7bf27e7a5c65b2b15e0a2600433b4f8b8f5b
Florian Boudinet <florian.boudinet@eshard.com>
2022-09-23 10:19:13 +0200

    [FIX] change order in test

--- scared/_utils.py
+++ scared/_utils.py
@@ -2,11 +2,10 @@ import numpy as _np
 
 
 def _is_bytes_array(array):
-    if array.dtype == 'uint8':
-        return True
-
     if not isinstance(array, _np.ndarray):
         raise TypeError(f'array should be a Numpy ndarray instance, not {type(array)}.')
+    if array.dtype == 'uint8':
+        return True
     if not (_np.alltrue(array >= 0) and _np.alltrue(array < 256)) or array.dtype.char not in _np.typecodes['AllInteger']:
         raise ValueError(f'array should be a byte array with uint8 or at least, between 0 and 256 dtype, not {array.dtype}.')
     return True
