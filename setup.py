34f19b18b6ff88339b74dbdd47b5819db1a8e600
Florian Boudinet <florian.boudinet@eshard.com>
2025-11-19 12:28:59 +0100

    [REWORK] remove versioneer to use setuptool-scm. Bump version

--- setup.py
+++ setup.py
@@ -1,33 +1,5 @@
-import versioneer
+from setuptools import setup
 
-from setuptools import setup, find_packages
-requirements = [
-    "numba",
-    "numpy>=1.20.3",
-    "estraces>=1.9.4",
-    "psutil",
-    "scipy"
-]
-
-
-setup(
-    name='scared',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="eshard scared Python library.",
-    author="eshard",
-    license="Proprietary",
-    author_email='contact@eshard.com',
-    packages=find_packages(include=["scared", "scared.*"]),
-    install_requires=requirements,
-    keywords='scared',
-    classifiers=[
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development',
-    ]
-)
+# Configuration is now in pyproject.toml
+# This file is kept for backward compatibility
+setup()
